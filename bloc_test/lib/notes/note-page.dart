7ba727f3b1e1d97390f5c9d25279d376a9308fd7
Roland Peres <rollyperesh@gmail.com>
2020-05-08 01:46:35 +0300

    Fix editable details

--- bloc_test/lib/notes/note-page.dart
+++ bloc_test/lib/notes/note-page.dart
@@ -6,21 +6,26 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class NotesPage extends StatelessWidget{
+import 'notes_bloc/bloc.dart';
+
+class NotesPage extends StatelessWidget {
   NotesPage();
 
   @override
   Widget build(BuildContext context) {
     return BlocProvider<NotesBloc>(
-      create: (context) => NotesBloc(notesRepository: FirebaseNotesRepository()),
+      create: (context) => NotesBloc(
+        notesRepository: FirebaseNotesRepository(),
+        // you can add the event here because the bloc will start with `NotesEmpty` state
+      )..add(GetNotes()),
       child: Scaffold(
         appBar: AppBar(
           title: Text('Notes'),
           centerTitle: true,
         ),
-        body:  NotesView(),
+        body: NotesView(),
         floatingActionButton: FloatingActionButton(
-          onPressed: (){
+          onPressed: () {
             Navigator.pushNamed(context, noteDetailsRoute, arguments: '');
           },
           tooltip: 'Add New Note',
--- bloc_test/lib/notes/notes-view.dart
+++ bloc_test/lib/notes/notes-view.dart
@@ -1,88 +1,84 @@
-
 import 'package:bloctest/constants.dart';
 import 'package:bloctest/notes/note.dart';
 import 'package:bloctest/notes/notes_bloc/bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:rxdart/rxdart.dart';
+import 'package:bloctest/shared/shared.dart';
 
-class NotesView extends StatefulWidget{
-  NotesView();
-  State<NotesView> createState() => _NotesViewState();
+class NotesView extends StatefulWidget {
+  @override
+  _NotesViewState createState() => _NotesViewState();
 }
 
 class _NotesViewState extends State<NotesView> {
-  NotesBloc _notesBloc;
-
-  @override
-  void initState() {
-    super.initState();
-    _notesBloc = BlocProvider.of<NotesBloc>(context);
-  }
-
   @override
   Widget build(BuildContext context) {
     return BlocListener<NotesBloc, NotesState>(
-        listener: (context, state) {
-        },
-        child: BlocBuilder<NotesBloc, NotesState>(
-            bloc: BlocProvider.of(context),
-            builder: (context, state){
-              if(state is NotesEmpty){
-                _notesBloc.add(GetNotes());
-                return Center(child: CircularProgressIndicator());
-              }
-              if(state is NotesLoadInProgress){
-                return Center(child: CircularProgressIndicator());
-              }else if(state is NotesLoadSuccess){
-                final notes = _sortNotes(state.notes, state.sortField);
-                return ListView.builder(
-                    shrinkWrap: true,
-                    physics: NeverScrollableScrollPhysics(),
-                    itemCount: notes.length,
-                    reverse: state.sortDirection == SortDirections.ASC ? true : false,
-                    itemBuilder: (context, position){
-                      final note = notes[position];
-                      return Card(
-                        child: Column(
-                          children: <Widget>[
-                            ListTile(
-                              leading: Icon(Icons.description, size: 40.0),
-                              title: Text('${note.title}'),
-                              subtitle: (note.dateCreated.day.compareTo(note.dateModified.day) == 0) ?
-                              Text('${DateFormat.yMMMd().format(note.dateModified)}') :
-                              Text('${DateFormat.yMMMd().format(note.dateModified)} / ${DateFormat.yMMMd().format(note.dateCreated)}', ),
-                              trailing: _popupMenu(note, position),
-                              onTap: (){
-                                Navigator.pushNamed(context, noteDetailsRoute, arguments: note.id);
-                              },
-                            ),
-                          ],
-                        ),
-                      );
-                    }
-                );
-              }else {
-                return Container();
-              }
+      listener: (context, state) {},
+      child: BlocBuilder<NotesBloc, NotesState>(
+          bloc: BlocProvider.of(context),
+          builder: (context, state) {
+            if (state is NotesEmpty || state is NotesLoadInProgress) {
+              return const Center(child: CircularProgressIndicator());
             }
-        ),
+            if (state is NotesLoadSuccess) {
+              final notes = _sortNotes(state.notes, state.sortField);
+              return ListView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: notes.length,
+                  reverse:
+                      state.sortDirection == SortDirections.ASC ? true : false,
+                  itemBuilder: (context, position) {
+                    final note = notes[position];
+                    return Card(
+                      child: Column(
+                        children: <Widget>[
+                          ListTile(
+                            leading: Icon(Icons.description, size: 40.0),
+                            title: Text('${note.title}'),
+                            subtitle: (note.dateCreated.day
+                                        .compareTo(note.dateModified.day) ==
+                                    0)
+                                ? Text(
+                                    '${DateFormat.yMMMd().format(note.dateModified)}')
+                                : Text(
+                                    '${DateFormat.yMMMd().format(note.dateModified)} / ${DateFormat.yMMMd().format(note.dateCreated)}',
+                                  ),
+                            trailing: _popupMenu(note, position),
+                            onTap: () {
+                              Navigator.pushNamed(context, noteDetailsRoute,
+                                  arguments: note.id);
+                            },
+                          ),
+                        ],
+                      ),
+                    );
+                  });
+            }
+            return const Empty();
+          }),
     );
   }
 
-  List<Note> _sortNotes(List<Note> notes, SortFields sortOption){
-    notes.sort((a, b){
-      switch(sortOption){
-        case SortFields.DATECREATED:{
-          return a.dateCreated.millisecondsSinceEpoch.compareTo(b.dateCreated.millisecondsSinceEpoch);
-        }
-        case SortFields.DATEMODIFIED:{
-          return a.dateModified.millisecondsSinceEpoch.compareTo(b.dateModified.millisecondsSinceEpoch);
-        }
-        case SortFields.TITLE:{
-          return a.title.toUpperCase().compareTo(b.title.toUpperCase());
-        }
+  List<Note> _sortNotes(List<Note> notes, SortFields sortOption) {
+    notes.sort((a, b) {
+      switch (sortOption) {
+        case SortFields.DATECREATED:
+          {
+            return a.dateCreated.millisecondsSinceEpoch
+                .compareTo(b.dateCreated.millisecondsSinceEpoch);
+          }
+        case SortFields.DATEMODIFIED:
+          {
+            return a.dateModified.millisecondsSinceEpoch
+                .compareTo(b.dateModified.millisecondsSinceEpoch);
+          }
+        case SortFields.TITLE:
+          {
+            return a.title.toUpperCase().compareTo(b.title.toUpperCase());
+          }
       }
       return a.title.compareTo(b.title);
     });
@@ -90,27 +86,26 @@ class _NotesViewState extends State<NotesView> {
   }
 
   Widget _popupMenu(Note note, int position) => PopupMenuButton<int>(
-    itemBuilder: (context) => [
-      PopupMenuItem(
-        value: 2,
-        child: Text(
-          "Delete"
-        ),
-      ),
-    ],
-    onSelected: (value) async{
-      if(value == 2){
-        _deleteAlert(note).then((res){});
-      }
-      print("value:$value");
-    },
-  );
+        itemBuilder: (context) => [
+          PopupMenuItem(
+            value: 2,
+            child: Text("Delete"),
+          ),
+        ],
+        onSelected: (value) async {
+          if (value == 2) {
+            final result = await _deleteAlert(note);
+          }
+          print("value:$value");
+        },
+      );
 
   Future<String> _deleteAlert(Note note) async {
     return showDialog<String>(
-      context: context,
+      context: context, // you need to use this context to get the bloc
       barrierDismissible: false, // user must tap button!
-      builder: (BuildContext context) {
+      builder: (_) {
+        // this dialog context(_) is different than the one above and won't find your bloc since it's basically a new route; any dialog gets pushed onto a new route.
         return AlertDialog(
           title: Text('Delete note?'),
           content: SingleChildScrollView(
@@ -124,7 +119,7 @@ class _NotesViewState extends State<NotesView> {
             FlatButton(
               child: Text('Delete'),
               onPressed: () {
-                _notesBloc.add(DeleteNote(note));
+                context.bloc<NotesBloc>().add(DeleteNote(note));
                 Navigator.pop(context, "Delete");
               },
             ),
@@ -140,5 +135,3 @@ class _NotesViewState extends State<NotesView> {
     );
   }
 }
-
-
